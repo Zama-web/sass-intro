0c4458b5e7b1231047179be1ad3aeea2b758d284
Zamir <baizakoff2017@gmail.com>
2020-04-16 22:13:45 +0300

    jQuery-test

---
+++ src/script/script.js
@@ -0,0 +1,75 @@
+$('.point').on('click', function () {
+    let text = $(this).text();
+    $('.main').text(text);
+})
+
+let link = 'https://medialeaks.ru/wp-content/uploads/2017/06/europe-01-1-524x419.jpg';
+let size = 100;
+let y = '50';
+let x = '50';
+
+$('.image').css('background-image', `url('${link}')`);
+
+
+
+$('.zoomIn').on('click', function () {
+    size += 10
+    $('.image').css('background-size', ++size + '%');
+})
+$('.zoomOut').on('click', function () {
+    size -= 10
+    $('.image').css('background-size', --size + '%');
+})
+
+
+$('.scroollUp').on('click', function () {
+    y -= 10
+    $('.image').css('background-position', `${50}% ${y}%`);
+})
+
+$('.scroollDown').on('click', function () {
+    y += 10
+    $('.image').css('background-position', `${50}% ${y}%`);
+})
+
+/////////////////////
+
+$('#block').on('click', function () {
+    $(this).animate({
+        left: '400px',
+        top: '400px',
+        backgroundColor: "yellow"
+    }, 2000);
+});
+
+//////////////////////
+
+$('#block2').click(function () {
+    $(this).animate({
+        width: "30%",
+        opacity: 0.4,
+        marginRight: "0.5in",
+        fontSize: "3em",
+        borderWidth: "10px",
+        backgroundColor: "green"
+    }, 2000);
+});
+
+//////////////////////
+
+$('#block3').on('mouseenter', function (e) {
+    var maxX = $(window).width() - $(this).width();
+    var maxY = $(window).height() - $(this).height();
+    $(this).css({
+        'left': getRandomInt(20, maxX),
+        'top': getRandomInt(20, maxY),
+        fontSize: "30px",
+        backgroundColor: "yellow"
+    });
+});
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+///////////////////////
